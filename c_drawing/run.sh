c0cf677a947f8bcb2bc6d5fde6012b77e7642385
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-10-13 14:34:10 +0200

    a

--- c_drawing/run.sh
+++ c_drawing/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-make && ./drawing
+
+gcc main.c drawing.c -o drawing -lm
+./drawing
+
 echo "TECHIO> open -s /project/target/ output.html"
--- math/test.c
+++ math/test.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int mail() {
+int main() {
 	float a;
 	
 	a = cos(3.14);
 	
 	printf("%f",a);
 	
+	return 0;
 }
