b63009a6e986b3f3313e0f0cb533e70c1bbfdb3e
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-10-14 12:49:48 +0200

    all

--- c_drawing/main.c
+++ c_drawing/main.c
@@ -1,44 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "drawing.h"
 
-void polygone(int, int);
-void flower1(int, int);
-void flower2(int, int);
-
 int main() {
-    int tour,i;
 
-    init_drawing(1000,550);
-
-	flower2(50,8);
-	
-    display_draw();
     return 0;
-}
-
-void flower1(int length, int side) {
-    int i;
-    for (i=1;i<=side;i++) {
-        polygone(length,3);
-        turn(360/side,LEFT);
-    }
-}
-
-void flower2(int length, int side) {
-    int i;
-    for (i=1;i<=side;i++) {
-        polygone(length,5);
-        draw(length);
-        turn(360/side,RIGHT);
-    }
-}
-
-void polygone(int length, int side) {
-    int i;
-    for (i=0;i<side;i++) {
-        draw(length);
-        turn(360/side,LEFT);
-    }
 }
